c36b8820d0254ceb8ddca79d0e353a7796c3b054
Raphael Stoeckli <github@rabanti.ch>
2017-11-07 02:11:22 +0100

    Update of parallax scene

--- scenes/ParallaxClouds.gd
+++ scenes/ParallaxClouds.gd
@@ -1,11 +1,14 @@
 #tool
 extends Node2D
 
-export(int, 0, 9999) var speed = 20
+export(float, 1.0, 9999.0) var speed = 20.0
 export(Texture) var image = preload("res://assets/imgs/clouds1.png")
 export(String, "RIGHT", "LEFT") var movement_direction = "RIGHT"
 export(Color) var color = Color(1,1,1)
 export(int, 0, 9999) var padding = 0
+#export(float, -999.0,999.0) var scale_y = 1.0
+#export(float, -999.0,999.0) var scale_x = 1.0
+export(Vector2) var scale = Vector2(1,1)
 
 export(Color) var placerholder_color = Color(0,0,0) setget set_placeholder_color
 
@@ -16,11 +19,12 @@ var texture_width = 0
 
 func set_placeholder_color(color):
 	placerholder_color = color
-	get_node("Placeholder").set_modulate(placerholder_color)
+	if get_node("Placeholder") != null:
+		get_node("Placeholder").set_modulate(placerholder_color)
 
 func _ready():
 	get_node("Placeholder").queue_free()
-	texture_width = image.get_width()
+	texture_width = image.get_width() * scale.x
 	screen_width = OS.get_window_size().width
 	number_of_elements = ceil(screen_width / texture_width) + 1
 	
@@ -28,7 +32,8 @@ func _ready():
 	
 	for i in range(0, number_of_elements):
 		var sprite = Sprite.new()
-		sprite.set_pos(Vector2(i * (texture_width + padding), 0))
+		sprite.set_pos(Vector2(i * (texture_width + padding) , 0))
+		sprite.set_scale(scale)
 		sprite.set_texture(image)
 		sprite.set_modulate(color)
 		add_child(sprite)
@@ -39,7 +44,9 @@ func _ready():
 
 
 func _process(delta):
-	var number_of_pixels_moved = ceil(delta * speed * direction)
+	var number_of_pixels_moved = (delta * speed * direction) #ceil((delta * speed * direction)/100)
+	if number_of_pixels_moved >= 1:
+		number_of_pixels_moved = ceil(number_of_pixels_moved)
 	for child in get_children():
 		if child extends Sprite:
 			var old_position = child.get_pos()
