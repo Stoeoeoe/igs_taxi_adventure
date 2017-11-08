84bf61f05d2031fe45708fc59b733298fcb6c391
Matthias Stöckli <matthias.stoe@gmail.com>
2017-11-08 22:38:55 +0100

    Added animation capabilities to blocks

--- classes/blockdata.gd
+++ classes/blockdata.gd
@@ -5,6 +5,12 @@ export(Color) var block_color = Color(1,1,1)
 export(Sample) var destruction_sample = null
 export(Texture) var overlay = null
 export(bool) var victory_relevant = true
+export(int) var number_of_hits = 1
+
+export(Texture) var background_sprite = null
+export(int) var animation_h_frames = 1
+export(int) var animation_v_frames = 1
+export(float) var animation_speed = 0
 
 
 func _init(id).(id):
--- scenes/Block.gd
+++ scenes/Block.gd
@@ -12,28 +12,29 @@ export(String, "regular", "indestructable", "rabium") var block_type = "regular"
 var score = 0
 
 func _ready():
-
 	block_data = data.get_item("blockdata", block_type)
+	sprite.set_texture(load(block_data.background_sprite))
+
+	sprite.set_hframes(block_data.animation_h_frames)
+	sprite.set_vframes(block_data.animation_v_frames)
+	if block_data.animation_h_frames > 1 or block_data.animation_v_frames > 1:
+		var frames = (block_data.animation_h_frames * block_data.animation_v_frames) - 1
+		create_base_sprite_animation(frames, block_data.animation_speed)	
 	self.score = block_data.score
+	sprite.set_modulate(block_data.block_color)
 	if block_data.overlay:
 		get_node("Overlay").set_texture(load(block_data.overlay))
 #	var sample_library = SampleLibrary.new()
 #	sample_player.set_sample_library(sample_library)
 #	sample_player.get_sample_library().add_sample("hit_sound", hit_sound)
 	
-	if block_type == "regular":
-		sprite.set_texture(preload("../assets/imgs/blocks/default_block.png"))
-	elif block_type == "indestructable":
-		sprite.set_texture(preload("../assets/imgs/blocks/indestructable_block.png"))
-	elif block_type == "rabium":
-		sprite.set_texture(preload("../assets/imgs/blocks/rabium_block.png"))
 
 func _on_block_hit( body ):
 	HUD.write(body.get_groups())
 	if body.is_in_group("ball"):
 		execute_block_action()
 		if block_type == "regular" or block_type == "rabium":
-			animation_player.play("DestructionAnim")
+			animation_player.play("DestructionAnimation")
 			
 			sample_player.play("block_destructable")
 			
@@ -41,9 +42,21 @@ func _on_block_hit( body ):
 			sample_player.play("block_indestructable")			
 
 func _on_AnimationPlayer_finished():
-	if animation_player.get_current_animation() == "DestructionAnim":
+	if animation_player.get_current_animation() == "DestructionAnimation":
 		hide()
 		queue_free()
+
+func create_base_sprite_animation(frames, speed):
+	var animation = animation_player.get_animation("BaseAnimation")
+	animation.set_length(speed)
+	animation.track_insert_key(0, 0, 0)
+	animation.track_insert_key(0, speed, frames + 1)
+	animation.set_loop(true)
+	animation_player.play("BaseAnimation")
+	
+	
+	
+
 
 func execute_block_action():
 	if block_data.victory_relevant:
