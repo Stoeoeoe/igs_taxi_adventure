b12d6b99e371824b581368efda27c40b6556f9a4
Matthias Stöckli <matthias.stoe@gmail.com>
2017-11-15 23:04:59 +0100

    Fancy Cutscene

--- scenes/cutscenes/Hyperspace.gd
+++ scenes/cutscenes/Hyperspace.gd
@@ -3,9 +3,14 @@ extends Node
 onready var bg = get_node("Backdrop")
 onready var lightning = get_node("Lightning")
 onready var animation_player = get_node("AnimationPlayer")
-onready var sound_player = get_node("TunderPlayer")
+onready var sound_player = get_node("ThunderPlayer")
+
+onready var flash_screen = get_node("FlashScreen")
+onready var ship = get_node("Ship/ShipSprite")
+onready var engine = get_node("Ship/Engine")
 
 var flash_length = 0.5
+var ship_flash_length = 0.15
 var flash_started = false
 var original_color = Color()
 var time_delta = 0.0
@@ -14,22 +19,11 @@ var lightning_pos = 0;
 var screen_width = 1024
 
 func _ready():
-	lightning.connect("flash_start", self, "flash_bg")
-	lightning.connect("flash_end", self, "move_lightning")
 	original_color = bg.get_modulate()
 	animation_player.play("Cutscene")
 	screen_width = OS.get_window_size().width
 	set_process(true)
 
-func flash_bg():
-	sound_player.play("thunder1")
-	bg.set_modulate(Color(1,1,1))
-	time_delta = 0.0
-	flash_started = true
-
-func move_lightning():
-	lightning_pos = rand_range(0, screen_width)
-	lightning.set_pos(Vector2(lightning_pos, lightning.get_pos().y))
 
 func _process(delta):
 	if flash_started:
@@ -39,6 +33,26 @@ func _process(delta):
 		if time_delta >= flash_length:
 			bg.set_modulate(original_color)
 			flash_started = false
+		if time_delta >= ship_flash_length:
+			ship.set_modulate(Color(1,1,1))
+			flash_screen.set_opacity(0)
+			engine.set_opacity(1)
 
 	
 	
+
+func _on_Lightning_flash_start():
+	if sound_player:
+		sound_player.play("thunder1")
+		bg.set_modulate(Color(1,1,1))
+		time_delta = 0.0
+		flash_started = true
+		flash_screen.set_opacity(1.0)
+		ship.set_modulate(Color(0,0,0))
+		engine.set_opacity(0)
+		
+
+
+func _on_Lightning_flash_end():
+	lightning_pos = rand_range(0, screen_width)
+	lightning.set_pos(Vector2(lightning_pos, lightning.get_pos().y))
