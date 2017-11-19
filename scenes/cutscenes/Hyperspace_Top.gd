9fdf591780ece977cc6fa8796b40c7a6c787196a
Raphael Stoeckli <github@rabanti.ch>
2017-11-19 21:06:29 +0100

    Update of cutscene

--- scenes/cutscenes/Hyperspace_Top.gd
+++ scenes/cutscenes/Hyperspace_Top.gd
@@ -1,7 +1,14 @@
 extends Node
 
 onready var animation_player = get_node("AnimationPlayer")
+onready var terminal = get_node("Terminal")
 
 func _ready():
 	animation_player.play("TopAnimation")
 	
+func pause_text():
+	animation_player.stop();
+
+func _on_StopButton_pressed():
+	terminal.clear()
+	animation_player.play("TopAnimation")
