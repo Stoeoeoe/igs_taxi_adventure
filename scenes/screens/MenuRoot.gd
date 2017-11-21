3d06b535e5a6536796ab6b38c026985554c121d8
Matthias Stöckli <matthias.stoe@gmail.com>
2017-11-21 21:31:42 +0100

    Deactivated menu interaction after game start

--- scenes/screens/MenuRoot.gd
+++ scenes/screens/MenuRoot.gd
@@ -7,14 +7,19 @@ onready var level_menu = get_node("LevelMenu")
 onready var sample_player = get_node("CentralSamplePlayer")
 onready var animation_player = get_node("AnimationPlayer")
 
+var interaction_possible = true
 
 func _ready():
 	HUD.hide_gameoverlay()
 	home_menu.connect("menu_selected", self, "_on_HomeMenu_menu_selected", [])
 
 func _on_HomeMenu_menu_selected( menu ):
+	if not interaction_possible:
+		return
+		
 	if menu == "save_the_universe":
 		animation_player.play("StartGame")
+		interaction_possible = false
 	elif menu == "settings":
 		settings_menu.show()
 		home_menu.hide()
